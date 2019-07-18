88e0cd02ddd02a39fbcb4da220d88fcf06323117
accorreia123 <a.correia003@gmail.com>
2019-07-18 13:41:10 +0100

    2st commit

--- app/src/main/java/com/acc/app79/MainActivity.java
+++ app/src/main/java/com/acc/app79/MainActivity.java
@@ -8,6 +8,8 @@ import android.content.pm.PackageManager;
 import android.content.pm.ResolveInfo;
 import android.os.Bundle;
 import android.speech.RecognizerIntent;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -16,61 +18,73 @@ import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
-    private static final int SPEAK_REQUEST=10;
-TextView  textViewValue;
+    private static final int SPEAK_REQUEST = 10;
+    TextView textViewValue;
+    Button buttonVoiceIntent;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        textViewValue=(TextView) findViewById(R.id.textViewValue);
+        textViewValue = (TextView) findViewById(R.id.textViewValue);
+         buttonVoiceIntent =(Button) findViewById(R.id.buttonVoiceIntent);
 
-
-        PackageManager packageManager=this.getPackageManager();
+        PackageManager packageManager = this.getPackageManager();
         List<ResolveInfo> listOfInformation = packageManager.queryIntentActivities(
                 new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH),
                 0
         );
-        if(listOfInformation.size()>0){
+        if (listOfInformation.size() > 0) {
 
             Toast.makeText(MainActivity.this, "Device does support Speech Recognition",
                     Toast.LENGTH_SHORT).show();
-            listenToTheUsersVice();
-        }else {
+            listenToTheUsersVoice();
+        } else {
             Toast.makeText(MainActivity.this, "Device does not support Speech Recognition",
                     Toast.LENGTH_SHORT).show();
         }
+
+        buttonVoiceIntent.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                listenToTheUsersVoice();
+            }
+        });
     }
 
-    private void listenToTheUsersVice(){
+    private void listenToTheUsersVoice() {
         Intent voiceIntent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
         Intent intent = voiceIntent.putExtra(RecognizerIntent.EXTRA_PROMPT, "Talk to me!");
         voiceIntent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL,
                 RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
-        voiceIntent.putExtra(RecognizerIntent.EXTRA_MAX_RESULTS,10);
-        startActivityForResult(voiceIntent,SPEAK_REQUEST);
+        voiceIntent.putExtra(RecognizerIntent.EXTRA_MAX_RESULTS, 10);
+        startActivityForResult(voiceIntent, SPEAK_REQUEST);
 
     }
 
     @Override
-    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+    protected void onActivityResult(int requestCode, int resultCode,
+                                    @Nullable Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-        if(requestCode==SPEAK_REQUEST && resultCode==RESULT_OK){
-            ArrayList<String> voiceWords=data.getStringArrayListExtra(
+        if (requestCode == SPEAK_REQUEST && resultCode == RESULT_OK) {
+            ArrayList<String> voiceWords = data.getStringArrayListExtra(
                     RecognizerIntent.EXTRA_RESULTS);
-            float[] confidLevels=data.getFloatArrayExtra(
+            float[] confidLevels = data.getFloatArrayExtra(
                     RecognizerIntent.EXTRA_CONFIDENCE_SCORES);
-            int index=0;
-            String st="";
-            for( String s : voiceWords){
+            int index = 0;
+            String st = "";
+            for (String s : voiceWords) {
 
-                if (confidLevels!=null && index < confidLevels.length){
-                    st+=s + " - "+confidLevels[index]+"\n";
+                if (confidLevels != null && index < confidLevels.length) {
+                    st += s + " - " + confidLevels[index] + "\n";
                     textViewValue.setText(st);
                 }
             }
 
         }
     }
+
+
+
 }
